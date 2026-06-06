d250727a9d2cf548c186dfef52fc04d4600dadb6
VedSharma7310 <ss0811086@gmail.com>
2026-06-07 01:45:50 +0530

    Update client.py

--- Jarvis/client.py
+++ Jarvis/client.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AQ.Ab8RN6Ke_VvEjTmK8AqQzMA1VxHB0g9wDeT6e3bUyyNaiHB2gA")
+client = genai.Client(api_key="Enter the API key here")
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
